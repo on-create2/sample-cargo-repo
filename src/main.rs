270063471983152b4813482765911935cc4e8000
fufuthebat <fufuthebat@gmail.com>
2025-04-20 16:37:26 -0400

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    use std::thread;
+    use std::time::{Duration, SystemTime, UNIX_EPOCH};
+    use std::sync::atomic::{AtomicBool, Ordering};
+    use std::sync::Arc;
+    use ctrlc;
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+        println!("Exiting...");
+    }).expect("Error setting Ctrl-C handler");
+
+    while running.load(Ordering::SeqCst) {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        
+        let secs = now.as_secs();
+        let hours = (secs % 86400) / 3600;
+        let minutes = (secs % 3600) / 60;
+        let seconds = secs % 60;
+        
+        println!("Current time: {:02}:{:02}:{:02}", hours, minutes, seconds);
+        
+        thread::sleep(Duration::from_secs(1));
+    }
 }
